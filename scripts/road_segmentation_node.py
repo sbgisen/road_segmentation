4f078922af75572f4ca2c456cf3bfc6c4dce1339
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2024-08-14 14:36:11 +0900

    Fix bug

--- scripts/road_segmentation_node.py
+++ scripts/road_segmentation_node.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env pipenv-shebang
 # -*- coding:utf-8 -*-
 
-# Copyright (c) 2023 SoftBank Corp.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-from road_segmentation.model_loader import predict
-from road_segmentation.model_loader import load_model
-from sensor_msgs.msg import Image
-from jsk_recognition_msgs.msg import RectArray
-from jsk_recognition_msgs.msg import ClassificationResult
-from cv_bridge import CvBridgeError
-from cv_bridge import CvBridge
-import tensorflow as tensorflow
-import rospy
-import numpy as np
-import cv2
 import os
 
-print("Python interpreter:", os.popen('which python3').read())
+import cv2
+import numpy as np
+import rospy
+import tensorflow as tensorflow
+from cv_bridge import CvBridge
+from cv_bridge import CvBridgeError
+from jsk_recognition_msgs.msg import ClassificationResult
+from sensor_msgs.msg import Image
 
+from road_segmentation.data_loader.display import create_mask
+from road_segmentation.model_loader import load_model
+from road_segmentation.model_loader import predict
 
 bridge = CvBridge()
 
@@ -59,6 +43,12 @@ class RoadSegmentationNode:
         self.model_path = rospy.get_param('~model_path', default_model_path)
         self.debug = rospy.get_param('~debug', False)
 
+        # Load the segmentation model
+        self.model = load_model(self.model_path)
+
+        # Get input image size from model
+        self.input_image_size = self.model.input_shape[1:3]  # (height, width)
+
         # Subscribe to the camera topic
         self.image_sub = rospy.Subscriber(self.camera_topic, Image, self.image_callback)
 
@@ -69,10 +59,21 @@ class RoadSegmentationNode:
         if self.debug:
             self.debug_image_pub = rospy.Publisher('/road_segmentation/debug_image', Image, queue_size=1)
 
-        # Load the segmentation model
-        self.model = load_model(self.model_path)
-
         rospy.loginfo("Road Segmentation Node Initialized")
+
+    def perform_segmentation(self, image):
+        # Resize the input image to the size expected by the model
+        input_image = cv2.resize(image, (self.input_image_size[1], self.input_image_size[0]))  # (width, height)
+        input_image = np.expand_dims(input_image, axis=0)
+        input_image = input_image / 255.0  # Normalize input
+
+        # Get the segmentation result (class map)
+        result = predict(self.model, input_image)
+
+        # Convert the result to a mask image
+        result_mask = create_mask(result).numpy()
+
+        return result, result_mask
 
     def image_callback(self, msg):
         try:
@@ -83,21 +84,13 @@ class RoadSegmentationNode:
             return
 
         # Perform segmentation on the received image
-        prediction = self.perform_segmentation(cv_image)
-        self.publish_result(prediction)
+        result, result_mask = self.perform_segmentation(cv_image)
+        self.publish_result(result)
 
         # If debug mode is enabled, publish the debug image
         if self.debug:
-            debug_image = self.create_debug_image(cv_image, prediction)
+            debug_image = self.create_debug_image(cv_image, result_mask)
             self.publish_debug_image(debug_image)
-
-    def perform_segmentation(self, image):
-        # Resize the input image and perform prediction using the model
-        input_image = cv2.resize(image, (256, 256))
-        input_image = np.expand_dims(input_image, axis=0)
-        result = predict(self.model, input_image)
-        result = cv2.resize(result[0], (image.shape[1], image.shape[0]))
-        return result
 
     def publish_result(self, result):
         # Publish the segmentation result
@@ -106,9 +99,19 @@ class RoadSegmentationNode:
 
         self.result_pub.publish(classification_result)
 
-    def create_debug_image(self, image, result):
-        # Create a debug image overlaying the result on the original image
-        debug_image = cv2.addWeighted(image, 0.7, result, 0.3, 0)
+    def create_debug_image(self, image, result_mask):
+        # Resize result_mask to match the original image size
+        result_mask = cv2.resize(result_mask, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+
+        # Overlay the segmentation result on the original image
+        frame2 = image / 2
+        frame2[result_mask == 1] += [0, 0, 0]
+        frame2[result_mask == 2] += [0.5, 0.5, 0]
+        frame2[result_mask == 3] += [0.2, 0.7, 0.5]
+        frame2[result_mask == 4] += [0, 0.5, 0.5]
+        frame2[result_mask == 5] += [0, 0, 0.5]
+        frame2[result_mask == 6] += [0.5, 0, 0]
+        debug_image = np.uint8(frame2)
         return debug_image
 
     def publish_debug_image(self, debug_image):
--- src/road_segmentation/data_loader/split_train_test.py
+++ src/road_segmentation/data_loader/split_train_test.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env pipenv-shebang
 # -*- coding:utf-8 -*-
-from data_loader import train_images, mask_images
 import shutil
+
+from .data_loader import mask_images
+from .data_loader import train_images
 
 train_images = train_images()
 mask_images = mask_images()
 
-train_num = int(len(train_images)*0.75)
+train_num = int(len(train_images) * 0.75)
 
 test_img = train_images[train_num:]
 test_label = mask_images[train_num:]
 
-test_img_path = ['../dataset/test_img/' +test_img[i].split("/")[-1] for i in range(len(test_img))]
-test_label_path = ['../dataset/test_label/' +test_label[i].split("/")[-1] for i in range(len(test_label))]
+test_img_path = ['../dataset/test_img/' + test_img[i].split("/")[-1] for i in range(len(test_img))]
+test_label_path = ['../dataset/test_label/' + test_label[i].split("/")[-1] for i in range(len(test_label))]
 
 for i in range(len(test_img)):
     shutil.move(test_img[i], test_img_path[i])
