24d4602a379a70d05ebb92cba123ebfaf8d52c44
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2024-08-26 18:35:13 +0900

    Change color

--- scripts/road_segmentation_node.py
+++ scripts/road_segmentation_node.py
@@ -148,7 +148,7 @@ class RoadSegmentationNode:
     def create_debug_image(self, image, result_mask):
         overlay_image = image / 2
         overlay_image[(result_mask == 0)] += [0, 0, 0]
-        overlay_image[(result_mask == 1)] += [50, 0, 50]
+        overlay_image[(result_mask == 1)] += [20, 0, 20]
         overlay_image[(result_mask == 2)] += [100, 100, 0]
         overlay_image[(result_mask == 3)] += [40, 140, 100]
         overlay_image[(result_mask == 4)] += [0, 100, 100]
